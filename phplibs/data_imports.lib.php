4f2cffff671e6521bf2bb140bad4d57bc51302b6
Cédric Girard <cedric@daneel.net>
2018-11-03 10:59:41 +0100

    automatique

--- phplibs/data_imports.lib.php
+++ phplibs/data_imports.lib.php
@@ -6,17 +6,20 @@ function DumpTableViaSSH($sServeur, $sTable, array $aChamps, $sWhere = '', $iNbH
 	$sExec = "ssh %s 'mysql -e \"select %s from %s%s\"'";
 	$sExec = sprintf($sExec, $sServeur, implode(',', $aChamps), $sTable, $sWhere);
 	$s = exec($sExec, $aResult, $iResult);
-	$aData = array();
-	foreach($aResult as $iLine => $sLine) {
-		if($iLine >= $iNbHeaders) {
-			$sLine = trim($sLine);
-			$a = array();
-			foreach(explode("\t", $sLine) as $id => $sValue) {
-				if($sValue === 'NULL') $a[$aChamps[$id]] = NULL;
-				else $a[$aChamps[$id]] = $sValue;
+	if($iResult == 0) {
+		$aData = array();
+		foreach($aResult as $iLine => $sLine) {
+			if($iLine >= $iNbHeaders) {
+				$sLine = trim($sLine);
+				$a = array();
+				foreach(explode("\t", $sLine) as $id => $sValue) {
+					if($sValue === 'NULL') $a[$aChamps[$id]] = NULL;
+					else $a[$aChamps[$id]] = $sValue;
+				}
+				$aData[] = $a;
 			}
-			$aData[] = $a;
 		}
+		return $aData;
 	}
-	return $aData;
+	else die("erreur ($iResult) : " . implode(' / ', $aResult) . "\n");
 }
