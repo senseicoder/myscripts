4f2139ce11d68f15dccc29dfe43bc61e828df01e
Cédric Girard <c.girard@epiconcept.fr>
2020-03-12 16:29:03 +0100

    automatique

--- phplibs/data_compare.lib.php
+++ phplibs/data_compare.lib.php
@@ -1,16 +1,45 @@
 <?php
 
-function AnalysesSQL($oDB, array $aAnalyses, $bDebug = FALSE)
+define('formeCLI', 'cli');
+define('formeJSON', 'json');
+
+
+function AnalysesSQL($oDB, array $aAnalyses, $bDebug = FALSE, $sForme = formeCLI)
 {
+	$sOut = '';
+	$aTab = array();
+
 	foreach($aAnalyses as $sLib => $sSQL) if( ! empty($sSQL)) {
 		$aResult = $oDB->Query($sSQL);
 		if(count($aResult)) {
 			$aDisplay = array();
-			foreach($aResult as $aRec) $aDisplay[] = ' * ' . trim($aRec['lib']);
+			foreach($aResult as $aRec) $aDisplay[] = trim($aRec['lib']);
 			if($bDebug) printf("=== SQL: %s\n", $sSQL);
-			printf("%s :\n%s\n", $sLib, implode("\n", $aDisplay));
+			switch($sForme)
+			{
+				case formeCLI: 
+					foreach($aDisplay as & $sValue) $sValue = ' * ' . $sValue;
+					$sOut .= printf("%s :\n%s\n", $sLib, implode("\n", $aDisplay)); 
+					break;	
+				case formeJSON: 
+					foreach($aDisplay as $sValue) {
+						$aExp = explode(' : ', $sValue);
+						$sUser = $aExp[0];
+						$sHosts = isset($aExp[1]) ? $aExp[1] : '';
+						$aTab[$sLib][$sUser] = explode(',', $sHosts);
+					}
+					break;	
+				default: die('forme non gérée : ' . $sForme);
+			}
 		}
 	}
+
+	switch($sForme)
+	{
+		case formeCLI: echo $sOut; break;	
+		case formeJSON: echo json_encode($aTab); break;	
+		default: die('forme non gérée : ' . $sForme);
+	}	
 }
 
 function RecodagesSQL($oDB, array $aAnalyses)
